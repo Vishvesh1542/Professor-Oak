32b3e9eefce48423db67d81d1e0b277dddc557d8
Vishvesh <95541010+Vishvesh1542@users.noreply.github.com>
2023-05-10 20:38:05 +0530

    Nothing

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import discord
 import interactions
+
+token = "MTEwNTg2NzQ4NTkwNDkxNjUxMA.GeVGBd.EIqurAJanZJM9pAQGQPIXXZj700O6Lx3kvJxO0"
+
+bot = interactions.Client(token=token, intents=interactions.Intents.ALL)
+
+# Test command
+@bot.command(name='test', description='None', scope=1084731374344359966)
+async def test(ctx: interactions.CommandContext):
+    print('test')
+
+
+
+bot.start(token=token)
