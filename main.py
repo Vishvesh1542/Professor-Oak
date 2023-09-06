c3bbfc11dfbb3e9cc2c4e2844c37c9d6adb1a695
Vishvesh Raj <rajvishvesh01@gmail.com>
2023-09-06 17:13:12 +0530

    Fixed Privacy Policy

--- main.py
+++ main.py
@@ -19,32 +19,32 @@ class ProfessorOak(discord.Bot):
         print(' [ INFO ]'.ljust(15) + f'Logged in as {self.user}')
 
     async def on_guild_join(self, guild):
-        owner = self.get_user(guild.owner_id)
-        owner.send("\
-Privacy Policy for Professor Oak\
-\
-1. Data Collection\
-\
-We want to assure you that Professor Oak does not collect or store any sensitive or personal information from users. We respect your privacy and prioritize the protection of your data.\
-\
-2. Server Data\
-\
-Our bot may collect and store server-specific data to provide its intended services, such as server id. However, this data is not linked to any individual user and is solely used for the bot's functionality within the server.\
-\
+        print(guild.owner_id)
+        owner = await self.fetch_user(guild.owner_id)
+        await owner.send("\
+Privacy Policy for Professor Oak\n\
+\n\
+1. Data Collection\n\
+\n\
+We want to assure you that Professor Oak does not collect or store any sensitive or personal information from users. We respect your privacy and prioritize the protection of your data.\n\
+\n\
+2. Server Data\n\
+\n\
+Our bot may collect and store server-specific data to provide its intended services, such as server id. However, this data is not linked to any individual user and is solely used for the bot's functionality within the server.\n\
+\n\
 3. Sensitive Information\
-\
-We do not request, access, or store sensitive information, including but not limited to personal identification data, passwords, or payment information. We have no access to private messages or conversations.\
-\
+\n\
+We do not request, access, or store sensitive information, including but not limited to personal identification data, passwords, or payment information. We have no access to private messages or conversations.\n\
+\n\
 4. Data Security\
-\
-The security of your data is essential to us. We have implemented measures to safeguard any server data stored by our bot. However, please be aware that no online service can guarantee absolute security.\
-\
-\
-5. Contact Information\
-\
-If you have any questions or concerns regarding our bot's privacy policy, please don't hesitate to contact me at known_as_agent.\
-\
-By using our Discord bot, you agree to the terms outlined in this privacy policy.\
+\n\
+The security of your data is essential to us. We have implemented measures to safeguard any server data stored by our bot. However, please be aware that no online service can guarantee absolute security.\n\
+\n\
+5. Contact Information\n\
+\n\
+If you have any questions or concerns regarding our bot's privacy policy, please don't hesitate to contact me at known_as_agent.\n\
+\n\
+By using our Discord bot, you agree to the terms outlined in this privacy policy.\n\
 ")
 
     async def on_message(self, message):
